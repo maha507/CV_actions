77d3e4ad225cc870b5cad599208994ef23ae83e6
maha507 <132257569+maha507@users.noreply.github.com>
2023-06-12 15:07:56 +0530

    a

--- src/App.svelte
+++ src/App.svelte
@@ -7,47 +7,48 @@
 	let gridData = [];
 	let isCVUploadPopupVisible = false;
 	let selectedRowData = null;
-
+  
 	async function uploadCV(file) {
-    // Perform further actions with the uploaded file
-
-    // Example: Update the backend API URL with the file upload
-    const formData = new FormData();
-    formData.append("file", file);
-
-    // Assuming you have the candidate ID available in `selectedRowData`
-    const uploadCandidateId = selectedRowData.id;
-
-    try {
-      const response = await fetch(
-        `https://api.recruitly.io/api/candidatecv/upload?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E&candidateId=${uploadCandidateId}`,
-        {
-          method: "POST",
-          body: formData,
-        }
-      );
-
-      if (response.ok) {
-        console.log("CV uploaded successfully!");
-        // Perform any additional actions upon successful upload
-      } else {
-        console.error("CV upload failed.");
-        // Handle the error accordingly
-      }
-    } catch (error) {
-      console.error("CV upload error:", error);
-      // Handle the error accordingly
-    }
-
-    // Close the CV upload popup
-    isCVUploadPopupVisible = false;
-  }
+	  // Perform further actions with the uploaded file
+  
+	  // Example: Update the backend API URL with the file upload
+	  const formData = new FormData();
+	  formData.append("file", file);
+  
+	  if (selectedRowData) {
+		const uploadCandidateId = selectedRowData.id; // Get the candidate ID from selectedRowData
+  
+		try {
+		  const response = await fetch(
+			`https://api.recruitly.io/api/candidatecv/upload?apiKey=TEST1236C4CF23E6921C41429A6E1D546AC9535E&candidateId=${uploadCandidateId}`,
+			{
+			  method: "POST",
+			  body: formData,
+			}
+		  );
+  
+		  if (response.ok) {
+			console.log("CV uploaded successfully!");
+			// Perform any additional actions upon successful upload
+		  } else {
+			console.error("CV upload failed.");
+			// Handle the error accordingly
+		  }
+		} catch (error) {
+		  console.error("CV upload error:", error);
+		  // Handle the error accordingly
+		}
+	  }
+  
+	  // Close the CV upload popup
+	  isCVUploadPopupVisible = false;
+	}
   
 	function handleSave() {
 	  // Perform save logic
 	  // In this case, we're updating the backend API URL in the handleSave function
 	  console.log("Save clicked");
-	  
+  
 	  // Close the CV upload popup
 	  isCVUploadPopupVisible = false;
 	}
@@ -55,7 +56,7 @@
 	function handleClose() {
 	  // Perform close logic
 	  console.log("Close clicked");
-	  
+  
 	  // Close the CV upload popup
 	  isCVUploadPopupVisible = false;
 	}
@@ -145,8 +146,7 @@
 			  saveRowChanges: "Save",
 			  cancelRowChanges: "Cancel",
 			  deleteRow: "Delete",
-			  confirmDeleteMessage:
-				"Are you sure you want to delete this record?",
+			  confirmDeleteMessage: "Are you sure you want to delete this record?",
 			},
 			onSaveRowChanges: handleSave, // Bind handleSave function to the saveRowChanges event
 		  },
@@ -174,37 +174,37 @@
   <div id="dataGrid"></div>
   
   {#if isCVUploadPopupVisible}
-	<div class="popup-overlay">
-	  <div class="popup-content">
-		<h3>Upload CV</h3>
-		<input
-		  type="file"
-		  on:change="{(event) => uploadCV(event.target.files[0])}"
-		  accept=".pdf,.doc,.docx"
-		/>
-		<button on:click={handleSave} class="btn btn-primary">Save</button>
-		<button on:click={handleClose} class="btn btn-secondary">Close</button>
-	  </div>
+  <div class="popup-overlay">
+	<div class="popup-content">
+	  <h3>Upload CV</h3>
+	  <input
+		type="file"
+		on:change="{(event) => uploadCV(event.target.files[0])}"
+		accept=".pdf,.doc,.docx"
+	  />
+	  <button on:click={handleSave} class="btn btn-primary">Save</button>
+	  <button on:click={handleClose} class="btn btn-secondary">Close</button>
 	</div>
+  </div>
   {/if}
   
   <style>
-	.popup-overlay {
-	  position: fixed;
-	  top: 0;
-	  left: 0;
-	  right: 0;
-	  bottom: 0;
-	  background-color: rgba(0, 0, 0, 0.5);
-	  display: flex;
-	  justify-content: center;
-	  align-items: center;
-	}
+  .popup-overlay {
+	position: fixed;
+	top: 0;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	background-color: rgba(0, 0, 0, 0.5);
+	display: flex;
+	justify-content: center;
+	align-items: center;
+  }
   
-	.popup-content {
-	  background-color: white;
-	  padding: 20px;
-	  border-radius: 4px;
-	}
+  .popup-content {
+	background-color: white;
+	padding: 20px;
+	border-radius: 4px;
+  }
   </style>
   
