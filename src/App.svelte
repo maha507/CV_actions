792e950dbf0f5c7d2b82b80e95c20e74f65d0f7f
maha507 <132257569+maha507@users.noreply.github.com>
2023-06-12 13:06:18 +0530

    a

--- src/App.svelte
+++ src/App.svelte
@@ -2,26 +2,26 @@
 	import { onMount } from "svelte";
 	import "bootstrap/dist/css/bootstrap.min.css";
 	import DevExpress from "devextreme";
-  
+	
 	let jsonData = [];
 	let gridData = [];
-  
+	
 	onMount(async () => {
 	  const response = await fetch(
 		"https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154"
 	  );
 	  const responseData = await response.json();
 	  jsonData = responseData.data;
-  
+	
 	  gridData = jsonData.map((item) => ({
 		id: item.id,
 		firstName: item.firstName,
-		surname: item.surname,
+		surname:item.surname,
 		email: item.email,
 		mobile: item.mobile,
 		cvUrl: item.cvUrl, // assuming cvUrl is the property containing the CV file URL
 	  }));
-  
+	
 	  const dataGrid = new DevExpress.ui.dxDataGrid(document.getElementById("dataGrid"), {
 		dataSource: gridData,
 		columns: [
@@ -32,8 +32,22 @@
 		  { dataField: "mobile", caption: "Mobile", width: 150 },
 		  {
 			caption: "Actions",
-			width: 200,
-			cellTemplate: function (container, options) {  
+			width: 400,
+			cellTemplate: function (container, options) {
+			  const editButton = document.createElement("button");
+			  editButton.innerText = "Edit";
+			  editButton.classList.add("btn", "btn-primary", "mr-2");
+			  editButton.addEventListener("click", function () {
+				dataGrid.editRow(options.rowIndex);
+			  });
+	
+			  const deleteButton = document.createElement("button");
+			  deleteButton.innerText = "Delete";
+			  deleteButton.classList.add("btn", "btn-danger");
+			  deleteButton.addEventListener("click", function () {
+				dataGrid.deleteRow(options.rowIndex);
+			  });
+	
 			  const cvUploadButton = document.createElement("button");
 			  cvUploadButton.innerText = "CV Upload";
 			  cvUploadButton.classList.add("btn", "btn-success", "mr-2");
@@ -42,7 +56,7 @@
 				// Implement CV upload logic here
 				console.log("CV Upload clicked for row:", rowData);
 			  });
-  
+	
 			  const cvDownloadButton = document.createElement("button");
 			  cvDownloadButton.innerText = "CV Download";
 			  cvDownloadButton.classList.add("btn", "btn-info", "mr-2");
@@ -51,7 +65,7 @@
 				// Implement CV download logic here
 				console.log("CV Download clicked for row:", rowData);
 			  });
-  
+	
 			  const viewCVButton = document.createElement("button");
 			  viewCVButton.innerText = "View CV";
 			  viewCVButton.classList.add("btn", "btn-secondary");
@@ -60,7 +74,7 @@
 				// Implement view CV logic here
 				console.log("View CV clicked for row:", rowData);
 			  });
-  
+	
 			  container.appendChild(editButton);
 			  container.appendChild(deleteButton);
 			  container.appendChild(cvUploadButton);
